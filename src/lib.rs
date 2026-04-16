3f8887e2b9bac7dc392f24363da212a02008bd99
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2026-04-16 21:07:17 +0100

    Allow up to 32 threads for DNS resolution

--- src/lib.rs
+++ src/lib.rs
@@ -149,6 +149,7 @@ fn run_with_exit_code() -> io::Result<i32> {
         name: "pwp".to_owned(),
         io_enabled: true,
         time_enabled: true,
+        max_blocking_threads: 32,
         ..Default::default()
     })?;
 
